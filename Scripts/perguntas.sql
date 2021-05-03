2085f0e8eab9135bde6914b1740698632f8692eb
vmaicon.araujo <vmaicon.araujo@gmail.com>
2021-05-03 04:18:39 -0500

    Adicionado respostas para as questões de 3 a 6

--- Scripts/perguntas.sql
+++ Scripts/perguntas.sql
@@ -12,16 +12,56 @@ WHERE area = 'Romance' AND ano < 2010;
 SELECT * from LIVRO l WHERE titulo = 'Cem Anos de Solidão';
 -- descobre quais vendas do livro
 SELECT * FROM VENDA WHERE CODLIVRO = 20;
--- retorna o nome do cliente que comprou o livro
-SELECT c.NOME FROM CLIENTE c INNER JOIN VENDA v on c.IDCLIENTE = v.CODCLIENTE AND v.CODLIVRO =20;
+-- Resposta: retorna o nome do cliente que comprou o livro
+SELECT c.NOME 
+FROM CLIENTE c 
+INNER JOIN VENDA v ON c.IDCLIENTE = v.CODCLIENTE 
+AND v.CODLIVRO =20;
 
 -- 3- Quais os livros  de autoria de  Machado de Assis?
 
+-- descobrir dados do autor na tabela autor
+SELECT * 
+FROM AUTOR 
+WHERE NOME = 'Machado de Assis';
+
+-- Resposta: retorna os os livros do autor
+SELECT l.TITULO FROM LIVRO l 
+INNER JOIN AUTORLIVRO al ON l.CODLIVRO = al.CODLIVRO 
+INNER JOIN AUTOR a ON a.CODAUTOR = al.CODAUTOR  
+AND a.NOME = 'Machado de Assis';
+
 -- 4- Qual o menor e maior preço de livros da área de  Tecnologia?
+
+-- Descobre os livros de tecnologia na tablea de livro
+SELECT * FROM LIVRO l WHERE AREA = 'Tecnologia';
+
+-- Resposta: retorna o menor e maior preço dos livros de tecnologia
+SELECT MIN(PRECO) AS MENOR_PREÇO, MAX(PRECO) AS MAIOR_PREÇO
+FROM LIVRO  
+WHERE AREA = 'Tecnologia';
 
 -- 5- Modifique o preço do livro 'Dom Casmurro' para 180 reais.
 
+-- Verifica se o livro existe
+SELECT * 
+FROM LIVRO 
+WHERE TITULO = 'Dom Casmurro';
+
+-- Resposta: altera o preço do livro de 100 para 180
+UPDATE LIVRO 
+SET PRECO = 180 
+WHERE CODLIVRO = 10;
+
 -- 6- Remova  o cliente 4000.
+
+-- Verifica se o cliente ID 4000 existe.
+SELECT * 
+FROM CLIENTE 
+WHERE IDCLIENTE = 4000;
+-- Resposta: remove o cliente ID 4000
+DELETE FROM CLIENTE 
+WHERE IDCLIENTE = 4000;
 
 -- 7- Quais os títulos dos livros da área de  Tecnologia?
 
