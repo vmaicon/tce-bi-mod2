d4574adfaf547d49d054b8da96c7a3d1a6112fc5
vmaicon.araujo <vmaicon.araujo@gmail.com>
2021-05-05 21:16:16 -0500

    Adicionado respostas para questões de 9 a 13

--- Scripts/perguntas.sql
+++ Scripts/perguntas.sql
@@ -84,14 +84,93 @@ FROM LIVRO
 WHERE EDITORA = 'Koogan';
 
 -- 9- Quantos livros de Tecnologia há?
+SELECT * FROM LIVRO l WHERE l.AREA = 'Tecnologia';
+
+SELECT l.AREA, COUNT(l.CODLIVRO) 
+FROM LIVRO l 
+WHERE l.AREA = 'Tecnologia' 
+GROUP BY l.AREA ;
 
 -- 10- Quais os nomes dos autores do livro Banco de Dados?
 
+-- Busca código do livro na tabela de livro
+SELECT * FROM LIVRO l WHERE l.TITULO = 'Banco de Dados';
+-- busca o código do AUTOR na tabela do autor;
+SELECT * FROM AUTOR a;
+-- verifica os códigos na tabela AUTORLIVRO  relacionados ao codigo do livro 
+-- correspondente ao titulo banco de dados
+SELECT * FROM AUTORLIVRO a2 WHERE CODLIVRO = 70;
+
+/*
+ * Resposta: 
+ * Faz o inner join de livro com autorlivro
+ * depois o inner join de autorlivro com autor
+ */
+SELECT * 
+FROM LIVRO l 
+INNER JOIN AUTORLIVRO al 
+ON l.CODLIVRO = al.CODLIVRO 
+INNER JOIN AUTOR a 
+ON al.CODAUTOR = a.CODAUTOR 
+WHERE l.TITULO = 'Banco de Dados' ;
+
 -- 11- Quais os nomes e endereços dos clientes de Xapuri?
+
+-- Resposta
+SELECT NOME FROM CLIENTE c WHERE ENDERECO LIKE '%XAPURI%';
 
 -- 12- Quais livros possuem mais de 2 vendas?
 
+/**
+ * Observando a tabela de vendas, no campo quantidade há 2 livros
+ * que em uma cada venda saíram duas quantidades
+ * códigos 20 e 30
+ */
+SELECT * FROM VENDA v2;
+
+/**
+ * Observando pela quantidade tuplas na tabela de vendas realizadas para cada livro
+ * não há mais de 1 venda por livro
+ */
+SELECT l.TITULO , COUNT(v.CODLIVRO) FROM LIVRO l
+INNER JOIN VENDA v 
+ON l.CODLIVRO = v.CODLIVRO 
+GROUP BY l.TITULO; 
+
+/**
+ * Buscando pela quantidade de tuplas na tabela de vendas, maior que 2 vendas
+ * também não há
+ */
+SELECT l.TITULO , COUNT(v.CODLIVRO) FROM LIVRO l
+INNER JOIN VENDA v 
+ON l.CODLIVRO = v.CODLIVRO 
+GROUP BY l.TITULO
+HAVING COUNT(l.CODLIVRO) > 2;
+
+/**
+ * Filtrando pelo campo quantidade onde esta seria superior a 2 vendas
+ * também não satisfaz a questão.
+ */
+SELECT l.TITULO , v.QUANTIDADE FROM LIVRO l
+INNER JOIN VENDA v 
+ON l.CODLIVRO = v.CODLIVRO
+WHERE v.QUANTIDADE > 2;
+
+-- Resposta sugerida: não há livros com mais de duas vendas
+
 -- 13- Quais livros nunca venderam?
+
+-- Resposta:
+/**
+ * A função isnull para a tabela de venda vai retonar apenas os dados
+ * que estão em livro, mas não estão em vendas
+ * 
+ * Onde livro é a tabela da esqueda e venda é a tabela da direita
+ */
+SELECT * FROM LIVRO 
+LEFT JOIN VENDA
+ON LIVRO.CODLIVRO = VENDA.CODLIVRO 
+WHERE ISNULL(VENDA.CODLIVRO) ; 
 
 -- 14- Quanto o cliente 3000 gastou em compra de livros?
 
